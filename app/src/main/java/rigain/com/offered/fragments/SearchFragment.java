0db29b06c2da5749a695d4c47fa70ffe5d58d89f
rituparno-gain <rituparno.gain86@gmail.com>
2018-03-12 11:11:37 -0700

    Search Activity and Watch List completed

--- app/src/main/java/rigain/com/offered/fragments/SearchFragment.java
+++ app/src/main/java/rigain/com/offered/fragments/SearchFragment.java
@@ -2,21 +2,51 @@ package rigain.com.offered.fragments;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import okhttp3.Credentials;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import rigain.com.offered.R;
 import rigain.com.offered.SearchFiltersActivity;
+import rigain.com.offered.models.ESHitsList;
+import rigain.com.offered.models.ESHitsObject;
+import rigain.com.offered.models.Post;
+import rigain.com.offered.util.ElasticSearchAPI;
+import rigain.com.offered.util.PostListAdapter;
+import rigain.com.offered.util.RecyclerViewMargin;
 
 /**
  * Created by rigain on 3/9/2018.
@@ -25,12 +55,23 @@ import rigain.com.offered.SearchFiltersActivity;
 public class SearchFragment extends Fragment {
 
     private static final String TAG = "SearchFragment";
+    private static final String BASE_URL = "http://35.225.202.225//elasticsearch/posts/post/";
+    private static final int NUM_GRID_COLUMNS = 3;
+    private static final int GRID_ITEM_MARGIN = 5;
 
     //widgets
     private ImageView mFilters;
     private EditText mSearchText;
     private FrameLayout mFrameLayout;
     private RecyclerView mRecyclerView;
+
+    //vars
+    private String mElasticSearchPassword;
+    private String mPrefCity;
+    private String mPrefStateProv;
+    private String mPrefCountry;
+    private ArrayList<Post> mPosts;
+    private PostListAdapter mAdapter;
     
     @Nullable
     @Override
@@ -46,6 +87,15 @@ public class SearchFragment extends Fragment {
         return view;
     }
 
+    private void setupPostsList(){
+        RecyclerViewMargin itemDecorator = new RecyclerViewMargin(GRID_ITEM_MARGIN, NUM_GRID_COLUMNS);
+        mRecyclerView.addItemDecoration(itemDecorator);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), NUM_GRID_COLUMNS);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mAdapter = new PostListAdapter(getActivity(), mPosts);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
     private void init(){
         mFilters.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,5 +105,144 @@ public class SearchFragment extends Fragment {
                 startActivity(intent);
             }
         });
+
+        mSearchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        ||actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+
+                    mPosts = new ArrayList<Post>();
+
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl(BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+
+                    ElasticSearchAPI searchAPI = retrofit.create(ElasticSearchAPI.class);
+
+                    HashMap<String, String> headerMap = new HashMap<String, String>();
+                    headerMap.put("Authorization", Credentials.basic("user", mElasticSearchPassword));
+
+                    String searchString = "";
+
+                    if(!mSearchText.equals("")){
+                        searchString = searchString + mSearchText.getText().toString() + "*";
+                    }
+                    if(!mPrefCity.equals("")){
+                        searchString = searchString + " city:" + mPrefCity;
+                    }
+                    if(!mPrefStateProv.equals("")){
+                        searchString = searchString + " state_province:" + mPrefStateProv;
+                    }
+                    if(!mPrefCountry.equals("")){
+                        searchString = searchString + " country:" + mPrefCountry;
+                    }
+
+                    Call<ESHitsObject> call = searchAPI.search(headerMap, "AND", searchString);
+
+                    call.enqueue(new Callback<ESHitsObject>() {
+                        @Override
+                        public void onResponse(Call<ESHitsObject> call, Response<ESHitsObject> response) {
+
+                            ESHitsList hitsList = new ESHitsList();
+                            String jsonResponse = "";
+                            try{
+                                Log.d(TAG, "onResponse: server response: " + response.toString());
+
+                                if(response.isSuccessful()){
+                                    hitsList = response.body().getHits();
+                                }else{
+                                    jsonResponse = response.errorBody().string();
+                                }
+
+                                Log.d(TAG, "onResponse: hits: " + hitsList);
+
+                                for(int i = 0; i < hitsList.getEsPostIndex().size(); i++){
+                                    Log.d(TAG, "onResponse: data: " + hitsList.getEsPostIndex().get(i).getPost().toString());
+                                    mPosts.add(hitsList.getEsPostIndex().get(i).getPost());
+                                }
+
+                                Log.d(TAG, "onResponse: size: " + mPosts.size());
+                                //setup the list of posts
+                                setupPostsList();
+
+                            }catch (NullPointerException e){
+                                Log.e(TAG, "onResponse: NullPointerException: " + e.getMessage() );
+                            }
+                            catch (IndexOutOfBoundsException e){
+                                Log.e(TAG, "onResponse: IndexOutOfBoundsException: " + e.getMessage() );
+                            }
+                            catch (IOException e){
+                                Log.e(TAG, "onResponse: IOException: " + e.getMessage() );
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<ESHitsObject> call, Throwable t) {
+                            Log.e(TAG, "onFailure: " + t.getMessage() );
+                            Toast.makeText(getActivity(), "search failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                return false;
+            }
+        });
+    }
+
+    public void viewPost(String postId){
+        ViewPostFragment fragment = new ViewPostFragment();
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+        Bundle args = new Bundle();
+        args.putString(getString(R.string.arg_post_id), postId);
+        fragment.setArguments(args);
+
+        transaction.replace(R.id.container, fragment, getString(R.string.fragment_view_post));
+        transaction.addToBackStack(getString(R.string.fragment_view_post));
+        transaction.commit();
+
+        mFrameLayout.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getFilters();
+    }
+
+    private void getElasticSearchPassword(){
+        Log.d(TAG, "getElasticSearchPassword: retrieving elasticsearch password.");
+
+        Query query = FirebaseDatabase.getInstance().getReference()
+                .child(getString(R.string.node_elasticsearch))
+                .orderByValue();
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot singleSnapshot = dataSnapshot.getChildren().iterator().next();
+                mElasticSearchPassword = singleSnapshot.getValue().toString();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getFilters(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mPrefCity = preferences.getString(getString(R.string.preferences_city), "");
+        mPrefStateProv = preferences.getString(getString(R.string.preferences_state_province), "");
+        mPrefCountry = preferences.getString(getString(R.string.preferences_country), "");
+
+        Log.d(TAG, "getFilters: got filters: \ncity: " + mPrefCity + "\nState/Prov: " + mPrefStateProv
+                + "\nCountry: " + mPrefCountry);
     }
 }
