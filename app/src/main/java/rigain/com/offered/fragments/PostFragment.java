0596f2adc9e6766f71a85adb983676d2f363147f
rituparno-gain <rituparno.gain86@gmail.com>
2018-03-11 23:25:09 -0700

    Elasticserach config and indexing cloud function implementation

--- app/src/main/java/rigain/com/offered/fragments/PostFragment.java
+++ app/src/main/java/rigain/com/offered/fragments/PostFragment.java
@@ -3,7 +3,9 @@ package rigain.com.offered.fragments;
 
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -16,9 +18,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import rigain.com.offered.R;
 import rigain.com.offered.SelectImageDialog;
+import rigain.com.offered.models.Post;
+import rigain.com.offered.util.RotateBitmap;
 import rigain.com.offered.util.UniversalImageLoader;
 
 /**
@@ -38,6 +56,8 @@ public class PostFragment extends Fragment implements SelectImageDialog.OnPhotoS
     //vars
     private Bitmap mSelectedBitmap;
     private Uri mSelectedUri;
+    private byte[] mUploadBytes;
+    private double mProgress = 0;
 
     @Nullable
     @Override
@@ -73,7 +93,161 @@ public class PostFragment extends Fragment implements SelectImageDialog.OnPhotoS
             }
         });
 
+        mPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to post");
+                if(!isEmpty(mTitle.getText().toString())
+                        && !isEmpty(mDescription.getText().toString())
+                        && !isEmpty(mPrice.getText().toString())
+                        && !isEmpty(mCountry.getText().toString())
+                        && !isEmpty(mStateProvince.getText().toString())
+                        && !isEmpty(mCity.getText().toString())
+                        && !isEmpty(mContactEmail.getText().toString())){
+
+                    //we have a bitmap and no Uri
+                    if(mSelectedBitmap != null && mSelectedUri == null){
+                        uploadNewPhoto(mSelectedBitmap);
+                    }
+                    //we have no bitmap and a uri
+                    else if(mSelectedBitmap == null && mSelectedUri != null){
+                        uploadNewPhoto(mSelectedUri);
+                    }
+                }else{
+                    Toast.makeText(getActivity(), "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+    private void uploadNewPhoto(Bitmap bitmap){
+        Log.d(TAG, "uploadNewPhoto: uploading a new image bitmap to storage");
+        BackgroundImageResize resize = new BackgroundImageResize(bitmap);
+        Uri uri = null;
+        resize.execute(uri);
+    }
+
+    private void uploadNewPhoto(Uri imagePath){
+        Log.d(TAG, "uploadNewPhoto: uploading a new image uri to storage.");
+        BackgroundImageResize resize = new BackgroundImageResize(null);
+        resize.execute(imagePath);
+    }
+
+    public class BackgroundImageResize extends AsyncTask<Uri, Integer, byte[]> {
+
+        Bitmap mBitmap;
+
+        public BackgroundImageResize(Bitmap bitmap) {
+            if(bitmap != null){
+                this.mBitmap = bitmap;
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(getActivity(), "Compressing Image", Toast.LENGTH_SHORT).show();
+            showProgressBar();
+        }
+
+        @Override
+        protected byte[] doInBackground(Uri... params) {
+            Log.d(TAG, "doInBackground: started.");
+
+            if(mBitmap == null){
+                try{
+                    RotateBitmap rotateBitmap = new RotateBitmap();
+                    mBitmap = rotateBitmap.HandleSamplingAndRotationBitmap(getActivity(),params[0]);
+                }catch (IOException e){
+                    Log.e(TAG, "doInBackground: IOException: " + e.getMessage());
+                }
+            }
+            byte[] bytes = null;
+            Log.d(TAG, "doInBackground: megabytes before compression: " + mBitmap.getByteCount() / 1000000 );
+            bytes = getBytesFromBitmap(mBitmap, 100);
+            Log.d(TAG, "doInBackground: megabytes before compression: " + bytes.length / 1000000 );
+            return bytes;
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            super.onPostExecute(bytes);
+            mUploadBytes = bytes;
+            hideProgressBar();
+            //execute the upload task
+            executeUploadTask();
+        }
+    }
+
+    private void executeUploadTask(){
+        Toast.makeText(getActivity(), "uploading image", Toast.LENGTH_SHORT).show();
+
+
+
+        final String postId = FirebaseDatabase.getInstance().getReference().push().getKey();
+
+        final StorageReference storageReference = FirebaseStorage.getInstance().getReference()
+                .child("posts/users/" + FirebaseAuth.getInstance().getCurrentUser().getUid() +
+                        "/" + postId + "/post_image");
+
+        UploadTask uploadTask = storageReference.putBytes(mUploadBytes);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(getActivity(), "Post Success", Toast.LENGTH_SHORT).show();
+
+                //insert the download url into the firebase database
+                Uri firebaseUri = taskSnapshot.getDownloadUrl();
+
+                Log.d(TAG, "onSuccess: firebase download url: " + firebaseUri.toString());
+
+                DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+                Post post = new Post();
+                post.setImage(firebaseUri.toString());
+                post.setCity(mCity.getText().toString());
+                post.setContact_email(mContactEmail.getText().toString());
+                post.setCountry(mCountry.getText().toString());
+                post.setDescription(mDescription.getText().toString());
+                post.setPost_id(postId);
+                post.setPrice(mPrice.getText().toString());
+                post.setState_province(mStateProvince.getText().toString());
+                post.setTitle(mTitle.getText().toString());
+                post.setUser_id(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                reference.child(getString(R.string.node_posts))
+                        .child(postId)
+                        .setValue(post);
+
+                resetFields();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Could not upload photo", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                double currentProgress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                if( currentProgress > (mProgress + 15)){
+                    mProgress = (100 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                    Log.d(TAG, "onProgress: upload is " + mProgress + "& done");
+                    Toast.makeText(getActivity(), mProgress + "%", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    public static byte[] getBytesFromBitmap(Bitmap bitmap, int quality){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality,stream);
+        return stream.toByteArray();
+    }
+
+
 
     private void resetFields(){
         UniversalImageLoader.setImage("", mPostImage);
--- app/src/main/java/rigain/com/offered/fragments/SearchFragment.java
+++ app/src/main/java/rigain/com/offered/fragments/SearchFragment.java
@@ -1,15 +1,22 @@
 package rigain.com.offered.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import rigain.com.offered.R;
+import rigain.com.offered.SearchFiltersActivity;
 
 /**
  * Created by rigain on 3/9/2018.
@@ -18,11 +25,35 @@ import rigain.com.offered.R;
 public class SearchFragment extends Fragment {
 
     private static final String TAG = "SearchFragment";
+
+    //widgets
+    private ImageView mFilters;
+    private EditText mSearchText;
+    private FrameLayout mFrameLayout;
+    private RecyclerView mRecyclerView;
     
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
+        mFilters = (ImageView) view.findViewById(R.id.ic_search);
+        mSearchText = (EditText) view.findViewById(R.id.input_search);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        mFrameLayout = (FrameLayout) view.findViewById(R.id.container);
+
+        init();
+
         return view;
+    }
+
+    private void init(){
+        mFilters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to filters activity.");
+                Intent intent = new Intent(getActivity(), SearchFiltersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
